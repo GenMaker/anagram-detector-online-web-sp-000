7f2c6665b3de861152022dbadd89d850d23cb47c
Panela Leung <leungpanela@gmail.com>
2019-09-02 17:26:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,28 @@
 # Your code goes here!
+# class Anagram
+#   attr_accessor :word
+#
+#   def initialize (word)
+#     @word = word
+#   end
+#
+#   def match (word_array)
+#     word_array.select do |word|
+#      word.split("").sort== @word.split("").sort
+#     end
+#   end
+#
+# end
 class Anagram
   attr_accessor :word
 
-  def initialize (word)
+  def initialize(word)
     @word = word
   end
 
   def match (word_array)
     word_array.select do |word|
-     word.split("").sort== @word.split("").sort
+    word.split("").sort ==@word.split("").sort
     end
   end
-
 end
